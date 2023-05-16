b56a91c4a19d077a052281ac4b06db94d112f57b
LFRezende <lfsrsprofessional@gmail.com>
2023-05-16 00:35:50 -0300

    Refined Interface

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let connectEl = document.getElementById("btnconnect");
+let noMM = document.getElementById("No-MM");
 
 async function connect() {
   if (typeof window.ethereum != "undefined") {
     console.log("There is an EVM-Based Wallet.");
     await window.ethereum.request({ method: "eth_requestAccounts" }); // Await to wait MM to be fully connected.
-    connectEl.innerHTML = "Connected!";
+    connectEl.innerHTML = "Connected";
   } else {
+    noMM.innerHTML = "Install Metamask to proceed.";
     console.log("No EVM-Based Wallet.");
   }
 }
