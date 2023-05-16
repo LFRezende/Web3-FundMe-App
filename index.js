a15a9f844652d36089c4c4a94b258febded2804f
LFRezende <lfsrsprofessional@gmail.com>
2023-05-16 01:55:59 -0300

    Added buttons to javascript

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import { ethers } from "./ethers-5.7.esm.min.js";
+
 let connectEl = document.getElementById("btnconnect");
+let fundEl = document.getElementById("fundbtn");
 let noMM = document.getElementById("No-MM");
+
+connectEl.onclick = connect;
+fundEl.onclick = fund;
 
 async function connect() {
   if (typeof window.ethereum != "undefined") {
@@ -9,5 +15,9 @@ async function connect() {
   } else {
     noMM.innerHTML = "Install Metamask to proceed.";
     console.log("No EVM-Based Wallet.");
+    // Added in HTML the type = module. This makes the calling of buttons impossible in HTML, they need to be done in the JS.
+    // Add them on the top of this script;
   }
 }
+
+async function fund() {}
